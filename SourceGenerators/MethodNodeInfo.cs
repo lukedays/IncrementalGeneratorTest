26bf84ab58b2576092e515eb1299583cf80e5e62
Lucas Dias <lucasgdias@gmail.com>
2024-06-12 19:59:18 -0300

    null defaults

--- SourceGenerators/MethodNodeInfo.cs
+++ SourceGenerators/MethodNodeInfo.cs
@@ -4,22 +4,22 @@ namespace SourceGenerators;
 
 public record MethodNodeInfo
 {
-    public string ClassName { get; set; }
-    public string ClassModifiers { get; set; }
+    public string ClassName { get; set; } = null!;
+    public string ClassModifiers { get; set; } = null!;
     public string? Namespace { get; set; }
-    public string MethodName { get; set; }
-    public ITypeSymbol ReturnType { get; set; }
-    public string ParamsDefinitions { get; set; }
-    public string ParamsCall { get; set; }
-    public string MethodModifiers { get; set; }
-    public string MethodAccessibility { get; set; }
-    public string Filename { get; set; }
-    public string MethodConstraints { get; set; }
-    public string MethodTypeParameters { get; set; }
-    public string ClassConstraints { get; set; }
-    public string ClassTypeParameters { get; set; }
+    public string MethodName { get; set; } = null!;
+    public ITypeSymbol ReturnType { get; set; } = null!;
+    public string ParamsDefinitions { get; set; } = null!;
+    public string ParamsCall { get; set; } = null!;
+    public string MethodModifiers { get; set; } = null!;
+    public string MethodAccessibility { get; set; } = null!;
+    public string Filename { get; set; } = null!;
+    public string MethodConstraints { get; set; } = null!;
+    public string MethodTypeParameters { get; set; } = null!;
+    public string ClassConstraints { get; set; } = null!;
+    public string ClassTypeParameters { get; set; } = null!;
     public double? CacheExpiration { get; set; }
     public string? DecoratorName { get; set; }
     public string? DecoratedMethodAccessibility { get; set; }
-    public string CacheKey { get; set; }
+    public string? CacheKey { get; set; }
 }
